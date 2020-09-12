57c17002e0037aeb1eb2c37b281d45e6c1738814
Steven Nelson <stevenjose.nelson.iics@ust.edu.ph>
2020-09-12 18:35:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/labprac4_group7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.labprac4_group7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
